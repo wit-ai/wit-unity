054ff647297d7c07a23341e9106501ff188060fd
Aaron Jackson <yolanother@gmail.com>
2021-06-05 00:09:19 -0700

    Add wit understanding viewer

---
+++ Scripts/Editor/BaseWitWindow.cs
@@ -0,0 +1,147 @@
+using System;
+using com.facebook.witai.data;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+namespace com.facebook.witai
+{
+    public abstract class BaseWitWindow : EditorWindow
+    {
+        public enum WindowStyles
+        {
+            Themed,
+            Editor
+        }
+
+        protected virtual WindowStyles WindowStyle => WindowStyles.Editor;
+
+        protected WitConfiguration[] witConfigs;
+        protected string[] witConfigNames;
+        protected int witConfigIndex = -1;
+        protected WitConfiguration witConfiguration;
+
+
+        protected virtual void OnEnable()
+        {
+            RefreshConfigList();
+        }
+
+        protected virtual void OnProjectChange()
+        {
+            RefreshConfigList();
+        }
+
+        protected void RefreshContent()
+        {
+            if (witConfiguration) witConfiguration.UpdateData();
+        }
+
+        protected void RefreshConfigList()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:WitConfiguration");
+            witConfigs = new WitConfiguration[guids.Length];
+            witConfigNames = new string[guids.Length];
+
+            for (int i = 0; i < guids.Length; i++) //probably could get optimized
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+                witConfigs[i] = AssetDatabase.LoadAssetAtPath<WitConfiguration>(path);
+                witConfigNames[i] = witConfigs[i].name;
+            }
+        }
+
+        protected virtual void OnGUI()
+        {
+            minSize = new Vector2(450, 300);
+            DrawHeader();
+
+            if (WindowStyle == WindowStyles.Themed)
+            {
+                GUILayout.BeginVertical(WitStyles.BackgroundWitBlue, GUILayout.ExpandWidth(true),
+                    GUILayout.ExpandHeight(true));
+            }
+            else
+            {
+                GUILayout.BeginVertical(GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+            }
+            OnDrawContent();
+            GUILayout.EndVertical();
+        }
+
+        protected abstract void OnDrawContent();
+
+        protected void DrawHeader(GUIStyle headerBackground = null)
+        {
+            GUILayout.BeginVertical(null == headerBackground ? WitStyles.BackgroundWhite25P : headerBackground);
+            GUILayout.Space(16);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(WitStyles.MainHeader, "Label"))
+            {
+                Application.OpenURL("https://wit.ai");
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(16);
+            GUILayout.EndVertical();
+        }
+
+        protected bool DrawWitConfigurationPopup()
+        {
+            if (null == witConfigs) return false;
+
+            bool changed = false;
+            if (witConfigs.Length == 1)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Wit Configuration");
+                EditorGUILayout.LabelField(witConfigNames[0], EditorStyles.popup);
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                var selectedConfig = EditorGUILayout.Popup("Wit Configuration", witConfigIndex, witConfigNames);
+                if (selectedConfig != witConfigIndex)
+                {
+                    witConfigIndex = selectedConfig;
+                    changed = true;
+                }
+            }
+
+            if (changed || witConfigs.Length > 0 && !witConfiguration)
+            {
+                if (witConfigIndex < 0)
+                {
+                    witConfigIndex = 0;
+                }
+                witConfiguration = witConfigs[witConfigIndex];
+                RefreshContent();
+            }
+
+            return changed;
+        }
+
+        protected void BeginCenter(int width = -1)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (width > 0)
+            {
+                GUILayout.BeginVertical(GUILayout.Width(width));
+            }
+            else
+            {
+                GUILayout.BeginVertical();
+            }
+        }
+
+        protected void EndCenter()
+        {
+            GUILayout.EndVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Scripts/Runtime/CallbackHandlers/MultiValueHandler.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using com.facebook.witai.lib;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.facebook.witai.callbackhandlers
+{
+    public class MultiValueHandler : WitResponseHandler
+    {
+        [Header("Intent Matching")]
+        [SerializeField] public string intent;
+        [Range(0, 1f)] [SerializeField] public float confidence = .6f;
+
+        [Header("Value Matching")]
+        [SerializeField] public string[] valuePaths;
+
+        [Header("Output")]
+        [Tooltip("Modify the string output, values can be inserted with {value} or {0}, {1}, {2}")]
+        [SerializeField] public string format;
+
+        [SerializeField] private ValueEvent onFormattedValueEvent = new ValueEvent();
+        [SerializeField] private MultiValueEvent onMultiValueEvent = new MultiValueEvent();
+
+        private WitResponseReference[] references;
+
+
+        private static Regex valueRegex = new Regex(Regex.Escape("{value}"), RegexOptions.Compiled);
+
+        private void Awake()
+        {
+            references = new WitResponseReference[valuePaths.Length];
+            for (int i = 0; i < valuePaths.Length; i++)
+            {
+                references[i] = WitResultUtilities.GetWitResponseReference(valuePaths[i]);
+            }
+        }
+
+        protected override void OnHandleResponse(WitResponseNode response)
+        {
+            var intentNode = response.GetFirstIntent();
+            if (intent == intentNode["name"].Value && intentNode["confidence"].AsFloat > confidence)
+            {
+                List<string> values = new List<string>();
+                for (int i = 0; i < references.Length; i++)
+                {
+                    var reference = references[i];
+                    var value = reference.GetStringValue(response);
+                    values.Add(value);
+                    if (!string.IsNullOrEmpty(format))
+                    {
+                        format = valueRegex.Replace(format, value, 1);
+                        format = format.Replace("{" + i + "}", value);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(format))
+                {
+                    onFormattedValueEvent.Invoke(format);
+                }
+
+                onMultiValueEvent.Invoke(values.ToArray());
+            }
+        }
+    }
+
+    [Serializable]
+    public class MultiValueEvent : UnityEvent<string[]> {}
+}
