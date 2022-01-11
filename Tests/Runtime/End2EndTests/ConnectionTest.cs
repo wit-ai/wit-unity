4a541c0c4b3f2355315ca311292ab24041f56279
Kamran Bigdely-Shamloo <kamranshamloo@fb.com>
2022-01-11 11:43:22 -0800

    Added end-to-end tests for timeout and and unclean termination.
    Summary: Added end-to-end tests for timeout and and unclean termination.
    
    Reviewed By: yolanother
    
    Differential Revision: D33513559
    
    fbshipit-source-id: c1eef0c2b67e26081146e1a05d40274ed746fb05

---
+++ Tests/Runtime/End2EndTests/ConnectionTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Facebook.WitAi.Lib;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Facebook.WitAi.Tests
+{
+    public abstract class ConnectionTest
+    {
+        public string endpoint = WitRequest.WIT_ENDPOINT_SPEECH;
+
+        public bool isSlowTest = false;
+
+        public bool IsSlowTest => isSlowTest;
+        protected string name;
+        public virtual string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public virtual bool Success { get; protected set; } = false;
+        public virtual bool TestComplete { get; protected set; } = false;
+        public virtual string TestResult { get; protected set; } = "";
+
+        public abstract string TestEndpoint { get; set; }
+
+        public void Execute(Wit wit)
+        {
+            OnExecute(wit);
+        }
+
+        protected abstract void OnExecute(Wit wit);
+
+        public abstract void OnResponse(WitResponseNode response);
+
+        public virtual void OnError(string code, string message)
+        {
+            TestFailed($"Failed: {code} - {message}");
+        }
+
+        public void Reset()
+        {
+            Success = false;
+            TestComplete = false;
+            OnReset();
+        }
+
+        protected virtual void OnReset()
+        {
+        }
+
+        public virtual void OnRequestCompleted()
+        {
+            TestComplete = true;
+        }
+
+        public virtual void OnRequestCreated(WitRequest request)
+        {
+            request.onRawResponse = OnRawResponse;
+            request.onCustomizeUri = uriBuilder =>
+            {
+                Uri uri;
+                if (uriBuilder.Path == endpoint)
+                {
+                    uri = new Uri($"http://localhost:3000/{TestEndpoint}");
+                }
+                else
+                {
+                    uri = uriBuilder.Uri;
+                }
+
+                return uri;
+            };
+        }
+
+        protected virtual void OnRawResponse(string response)
+        {
+            Debug.Log("Raw response: " + response);
+        }
+
+        public void TestPassed()
+        {
+            Success = true;
+        }
+
+        public void TestFailed(string reason)
+        {
+            TestResult = reason;
+            Success = false;
+        }
+    }
+}
---
+++ Tests/Runtime/End2EndTests/Message1024BoundaryTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Facebook.WitAi.CallbackHandlers;
+using Facebook.WitAi.Lib;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Facebook.WitAi.Tests
+{
+    public class Message1024BoundaryTest : ConnectionTest
+    {
+        [SerializeField] public string testEndpoint;
+        [SerializeField] public string testName;
+        [SerializeField] public ExpectedResult[] expectedResults;
+        public override string Name => testName;
+        public override string TestEndpoint
+        {
+            get
+            {
+                return testEndpoint;
+            }
+            set
+            {
+                testEndpoint = value;
+            }
+        }
+
+        protected override void OnExecute(Wit wit)
+        {
+            wit.ActivateImmediately();
+        }
+
+        public override void OnResponse(WitResponseNode response)
+        {
+            for (int i = 0; i < expectedResults.Length; i++)
+            {
+                var pathRef = WitResultUtilities.GetWitResponseReference(expectedResults[i].path);
+                var value = pathRef.GetStringValue(response);
+                var expected = expectedResults[i].value;
+                
+                if (value != expected)
+                {
+                    TestFailed($"Expected: {expected}, got: {value}");
+                    return;
+                }
+            }
+
+            TestPassed();
+        }
+    }
+
+    [Serializable]
+    public class ExpectedResult
+    {
+        public string path;
+        public string value;
+    }
+}
