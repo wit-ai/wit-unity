5f7aec0927804ea6a55eb8e1215ed55966aa0e50
Aaron Jackson <akjackson@fb.com>
2022-03-03 14:53:03 -0800

    Add missing copyright headers
    Summary: Adds missing copyright headers
    
    Reviewed By: ryanbailey-fb
    
    Differential Revision: D34625879
    
    fbshipit-source-id: 74f796b1b34741d7e9f5f1a2d478ac6ca5751603

--- Scripts/Runtime/Data/Entities/WitEntityData.cs
+++ Scripts/Runtime/Data/Entities/WitEntityData.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
 using Facebook.WitAi.Lib;
 using UnityEngine;
 
