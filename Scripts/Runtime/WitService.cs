f87e8bba46ab6fdb176717daa82f80cde8b8170c
Azis Senoaji Prasetyotomo <azisseno@fb.com>
2022-08-25 12:44:46 -0700

    Fix redundant import on WitService.cs
    Summary: as Title
    
    Reviewed By: nishsinghal20
    
    Differential Revision: D39025965
    
    fbshipit-source-id: eb62f7d39be4577784e8ee6674988b00dfd5c8e2

--- Scripts/Runtime/WitService.cs
+++ Scripts/Runtime/WitService.cs
@@ -11,7 +11,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.IO;
 using System.Net;
 using Facebook.WitAi.Configuration;
 using Facebook.WitAi.Data;
