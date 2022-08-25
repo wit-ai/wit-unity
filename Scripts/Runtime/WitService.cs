c176487716e3ef2930413817f715691e927d8408
Azis Senoaji Prasetyotomo <azisseno@fb.com>
2022-08-25 07:48:33 -0700

    Add server version to unity side and fix openXR issue
    Summary:
    Add server version configuration into wit runtime configuration
    
    And fixing the issue FileStream not found on openXR project
    
    https://pxl.cl/2bhbG
    
    Differential Revision: D38841228
    
    fbshipit-source-id: b16002b9ad180e56cc7543fdaf9b90cb241f1170

--- Scripts/Runtime/WitService.cs
+++ Scripts/Runtime/WitService.cs
@@ -11,6 +11,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.IO;
 using System.Net;
 using Facebook.WitAi.Configuration;
 using Facebook.WitAi.Data;
