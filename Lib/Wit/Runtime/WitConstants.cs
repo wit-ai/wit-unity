be2b29c5df1fddcb357aa767db1d769d228d8823
Ryan Bailey <rybailey@meta.com>
2023-01-27 06:40:00 -0800

    v52 Version Update
    Summary: Updates all desired version files
    
    Reviewed By: yolanother
    
    Differential Revision:
    D42754306
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: e9f14d4568984eb06afeccbfc5391567d557e863

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20221114";
-        public const string SDK_VERSION = "51.0.0";
+        public const string API_VERSION = "20230113";
+        public const string SDK_VERSION = "52.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
