567e52b14dad5082806cf8fd257c68d329d9bc9d
Ryan Bailey <rybailey@meta.com>
2023-02-23 08:08:46 -0800

    Updating to v53
    Summary:
    1. Set sdk to 53.0.0
    2. Set api to 20230213
    3. Run version script
    
    Reviewed By: yolanother
    
    Differential Revision:
    D43513790
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 4ae020b38643ef8a307a1113f3f8d67d37224a0a

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20230113";
-        public const string SDK_VERSION = "52.0.0";
+        public const string API_VERSION = "20230213";
+        public const string SDK_VERSION = "53.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
