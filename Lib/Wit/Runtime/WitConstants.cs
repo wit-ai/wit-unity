1050dc21191a99f3ce3475e527cd8b9bfee24233
Ryan Bailey <rybailey@meta.com>
2023-04-14 16:38:04 -0700

    v55 Version Update
    Summary:
    1. Update sdk version to v55 in WitConstant.cs
    2. Update api version to 20230413 in WitContants.cs
    3. Auto-update AppVoiceExperience & AppDictationExperience VERSION
    4. Auto-update all package.json versions
    
    Differential Revision:
    D44961705
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 26e0f183ae86a3dad51f589b5079ac00ddaf311b

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20230313";
-        public const string SDK_VERSION = "54.0.0";
+        public const string API_VERSION = "20230413";
+        public const string SDK_VERSION = "55.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
