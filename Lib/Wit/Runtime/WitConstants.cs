68c8e8fcf1fd9d69b3f457b35c5beceef4916351
Aaron Jackson <akjackson@meta.com>
2023-05-11 08:51:01 -0700

    Add NPC speech center
    Summary: Adds a new speech center to handle NPC specific TTS processes. NPC should have more control over its speech output in general. We want to abstract some of that away from TTS Speaker so it can remain an atomic unit. With this new module, rather than the user interacting directly with a speaker they're sending requests to the brain's speech center to handle these requests accordingly. For example, this will give us better control over the state when we're receiving multiple speech requests from LLM over time.
    
    Reviewed By: Barbog
    
    Differential Revision:
    D45656465
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 6e2bd3f3412db46fcbd1bc28feefe575240bc824

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,7 +11,7 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20230413";
+        public const string API_VERSION = "20230503";
         public const string SDK_VERSION = "55.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
