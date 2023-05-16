bf0fd2b69bb15125d1e39b2d6f7fd56d84eef2e4
ByMedion <dan.ef1999@gmail.com>
2023-05-16 22:08:11 +0300

    WitConfigurationUtility.GetLoadedConfigurations() fixed error for case when some scenes are not loaded in "Hierarchy"

--- Scripts/Editor/Data/Configuration/WitConfigurationUtility.cs
+++ Scripts/Editor/Data/Configuration/WitConfigurationUtility.cs
@@ -118,6 +118,11 @@ namespace Meta.WitAi.Data.Configuration
             for (int sceneIndex = 0; sceneIndex < SceneManager.sceneCount; sceneIndex++)
             {
                 Scene scene = SceneManager.GetSceneAt(sceneIndex);
+                if (!scene.isLoaded)
+                {
+                    continue;
+                }
+
                 foreach (var rootGameObject in scene.GetRootGameObjects())
                 {
                     IWitConfigurationProvider[] providers = rootGameObject.GetComponentsInChildren<IWitConfigurationProvider>(true);
