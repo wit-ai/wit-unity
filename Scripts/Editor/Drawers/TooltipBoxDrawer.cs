7047fbff7ea07418e9cdfda1deb62d188bbb0f59
Aaron Jackson <akjackson@meta.com>
2023-06-30 11:32:32 -0700

    Remove width calculation
    Summary: In Unity 2022, the GetHeight method in the DecoratorDrawers appears to be getting called outside of OnGUI. The view width calculation is required to be done in OnGUI and was causing exceptions. Moving the width calculation to be cached in the OnGUI call and prevent these errors.
    
    Reviewed By: JLeybovichMeta
    
    Differential Revision: D47128757
    
    fbshipit-source-id: 6a584f46ca42b2a03835faa7c40c2d9738eb5181

--- Scripts/Editor/Drawers/TooltipBoxDrawer.cs
+++ Scripts/Editor/Drawers/TooltipBoxDrawer.cs
@@ -16,34 +16,36 @@ namespace Meta.WitAi.Drawers
     [CustomPropertyDrawer(typeof(TooltipBoxAttribute))]
     public class TooltipBoxDrawer : DecoratorDrawer
     {
-        private float spaceAfterBox = 4;
-        private float iconSize = 32;
+        private float _spaceAfterBox = 4;
+        private float _iconSize = 32;
+        private float _lastViewWidth;
         
         public override float GetHeight()
         {
             if (!WitWindow.ShowTooltips) return 0;
             
             TooltipBoxAttribute infoBoxAttribute = (TooltipBoxAttribute)attribute;
-            var height = EditorStyles.helpBox.CalcHeight(new GUIContent(infoBoxAttribute.Text), EditorGUIUtility.currentViewWidth - iconSize);
-            return Mathf.Max(iconSize, height) + spaceAfterBox;
+            var height = EditorStyles.helpBox.CalcHeight(new GUIContent(infoBoxAttribute.Text), _lastViewWidth - _iconSize);
+            return Mathf.Max(_iconSize, height) + _spaceAfterBox;
         }
         
         public override void OnGUI(Rect position)
         {
             if (!WitWindow.ShowTooltips) return;
+            _lastViewWidth = EditorGUIUtility.currentViewWidth;
             
             var iconRect = EditorGUI.IndentedRect(position);
-            iconRect.width = iconSize;
-            iconRect.height = iconSize;
+            iconRect.width = _iconSize;
+            iconRect.height = _iconSize;
             GUIContent infoIcon = EditorGUIUtility.IconContent("console.infoicon");
             infoIcon.tooltip = "You can turn off these tooltips in Voice SDK Settings.";
             EditorGUI.LabelField(iconRect, infoIcon);
             
             var tooltip = (TooltipBoxAttribute) attribute;
             var rect = EditorGUI.IndentedRect(position);
-            rect.x += iconSize;
-            rect.width -= iconSize;
-            rect.height -= spaceAfterBox;
+            rect.x += _iconSize;
+            rect.width -= _iconSize;
+            rect.height -= _spaceAfterBox;
             EditorGUI.TextArea(rect, tooltip.Text, EditorStyles.helpBox);
         }
     }
