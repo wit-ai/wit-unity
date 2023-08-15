5c03ebdd0292e7551ef597722653b8c102f1d648
Aaron Jackson <akjackson@meta.com>
2023-08-15 15:11:36 -0700

    Create Button Attribute drawer
    Summary: Creates a new [Button] attribute for quick functional buttons in inspector UI.
    
    Reviewed By: ryanbailey-fb
    
    Differential Revision:
    D48324638
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 0d500a36b8dbaa6521184409aa1ccf6fdc138339

---
+++ Scripts/Editor/Drawers/ButtonAttributeDrawer.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using Meta.WitAi.Attributes;
+using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Meta.WitAi.Drawers
+{
+
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    public class ButtonAttributeDrawer : Editor
+    {
+        private Dictionary<string, bool> foldouts = new Dictionary<string, bool>();
+        private Dictionary<string, object[]> methodParameters = new Dictionary<string, object[]>();
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            MonoBehaviour monoBehaviour = target as MonoBehaviour;
+
+            foreach (var method in monoBehaviour.GetType()
+                         .GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                // Check if the method has our Button attribute
+                var buttonAttributes = method.GetCustomAttributes(typeof(ButtonAttribute), true);
+                if (buttonAttributes.Length > 0)
+                {
+                    var buttonAttribute = buttonAttributes[0] as ButtonAttribute;
+                    string buttonName = buttonAttribute.displayName ?? method.Name;
+
+                    if (!foldouts.ContainsKey(buttonName))
+                    {
+                        foldouts[buttonName] = false;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    
+                    if (!methodParameters.TryGetValue(buttonName, out var parameters))
+                    {
+                        parameters = new object[method.GetParameters().Length];
+                        methodParameters[buttonName] = parameters;
+                    }
+                    foldouts[buttonName] = EditorGUILayout.Foldout(foldouts[buttonName], buttonName);
+                    
+
+                    if (GUILayout.Button(buttonName))
+                    {
+                        method.Invoke(monoBehaviour, parameters);
+                    }
+                    GUILayout.EndHorizontal();
+
+                    if (foldouts[buttonName])
+                    {
+                        EditorGUILayout.BeginVertical("box");
+
+                        int paramIndex = 0;
+                        foreach (var parameter in method.GetParameters())
+                        {
+                            if (parameter.ParameterType == typeof(int))
+                            {
+                                parameters[paramIndex] = EditorGUILayout.IntField(parameter.Name,
+                                    (int)(parameters[paramIndex] ?? 0));
+                            }
+                            else if (parameter.ParameterType == typeof(float))
+                            {
+                                parameters[paramIndex] = EditorGUILayout.FloatField(parameter.Name,
+                                    (float)(parameters[paramIndex] ?? 0f));
+                            }
+                            else if (parameter.ParameterType == typeof(string))
+                            {
+                                parameters[paramIndex] =
+                                    EditorGUILayout.TextField(parameter.Name, (string)parameters[paramIndex]);
+                            }
+                            // ... add more types as needed
+
+                            paramIndex++;
+                        }
+
+                        EditorGUILayout.EndVertical();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Runtime/Attributes/ButtonAttribute.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace Meta.WitAi.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ButtonAttribute : Attribute
+    {
+        public readonly string displayName;
+
+        public ButtonAttribute(string displayName = null)
+        {
+            this.displayName = displayName;
+        }
+    }
+}
