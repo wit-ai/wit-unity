53aad4933e0b77442a38cbbda6f4b4ca92ac3722
Ryan Bailey <rybailey@meta.com>
2023-10-12 18:29:33 -0700

    Adds ITTSEventPlayer interface for custom tts event playback
    Summary:
    * Adds ITTSEventPlayer interface for sample playback broadcasting
    
    Documentation: https://docs.google.com/document/d/1t0LD_FbvqSfsRWpCO9STtT47wTY0C9XUPJDcuEBdCnw/edit?usp=sharing
    
    Reviewed By: nishsinghal20
    
    Differential Revision: D48605831
    
    fbshipit-source-id: b5398ab7c5fca0b8e9b211127fd04444dbf9fbc4

---
+++ Features/TTS/Scripts/Runtime/Interfaces/ITTSEventPlayer.cs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using Meta.WitAi.TTS.Data;
+
+namespace Meta.WitAi.TTS.Interfaces
+{
+    /// <summary>
+    /// Delegate that receives the latest sample index on sample change
+    /// </summary>
+    public delegate void TTSEventSampleDelegate(int newSample);
+
+    /// <summary>
+    /// An interface to be implemented for any script that can perform audio event playback
+    /// across the lifecycle of an audio file.
+    /// </summary>
+    public interface ITTSEventPlayer
+    {
+        /// <summary>
+        /// The current sample of the playing tts audio data if applicable
+        /// </summary>
+        int CurrentSample { get; }
+
+        /// <summary>
+        /// The total samples available for the events
+        /// </summary>
+        int TotalSamples { get; }
+
+        /// <summary>
+        /// The callback following a change to the current sample
+        /// </summary>
+        TTSEventSampleDelegate OnSampleUpdated { get; set; }
+
+        /// <summary>
+        /// The current TTS events if applicable
+        /// </summary>
+        TTSEventContainer CurrentEvents { get; }
+
+        /// <summary>
+        /// The callback following a event update
+        /// </summary>
+        TTSEventContainerDelegate OnEventsUpdated { get; set; }
+    }
+}
