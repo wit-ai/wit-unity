6ba125e610743fb80430e4bb7d37494b2ac80675
Ryan Bailey <rybailey@meta.com>
2023-10-12 18:29:33 -0700

    Adding TTSEventAnimator base class for lerping between two TTS events
    Summary:
    * Adds abstract TTSEventAnimator class that can be overwritten for performing custom animations as an ITTSAudioPlayer plays an audio clip
    * Adds an implementation with VisemeLipSyncTextureFlip which animates between Viseme mouth textures
    
    Reviewed By: Barbog
    
    Differential Revision: D49782007
    
    fbshipit-source-id: df8f37869a26c36df206779dd3d71a90776915fa

---
+++ Features/TTS/Scripts/Runtime/Integrations/TTSEventAnimator.cs
@@ -0,0 +1,249 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Meta.WitAi.Attributes;
+using Meta.WitAi.TTS.Data;
+using Meta.WitAi.TTS.Interfaces;
+
+namespace Meta.WitAi.TTS.Integrations
+{
+    /// <summary>
+    /// A base class for performing audio event based animations
+    /// </summary>
+    public abstract class TTSEventAnimator<TEvent, TData> : MonoBehaviour
+        where TEvent : TTSEvent<TData>
+    {
+        /// <summary>
+        /// The audio event player being used for playback
+        /// </summary>
+        public ITTSEventPlayer Player => _player as ITTSEventPlayer;
+        [SerializeField, ObjectType(typeof(ITTSEventPlayer))]
+        private UnityEngine.Object _player;
+
+        /// <summary>
+        /// Whether easing should be skipped & values should just be set
+        /// </summary>
+        public bool easeIgnored = false;
+
+        /// <summary>
+        /// The easing curve to be used for event lerping
+        /// </summary>
+        public AnimationCurve easeCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+        /// <summary>
+        /// The current TTS events being used for animation
+        /// </summary>
+        public List<TEvent> Events { get; private set; }
+
+        // Local sample counter
+        private int _sample = -1;
+        // Event placeholders
+        private TEvent _minEvent;
+        private TEvent _maxEvent;
+
+        /// <summary>
+        /// On awake, find IAudioEventPlayer for playback handling
+        /// </summary>
+        protected virtual void Awake()
+        {
+            // Find player if possible
+            if (Player == null)
+            {
+                _player = gameObject.GetComponentInChildren(typeof(ITTSEventPlayer));
+                if (Player == null)
+                {
+                    VLog.E($"No Player found for {GetType().Name}");
+                }
+            }
+            // Generate min & max events
+            _minEvent = Activator.CreateInstance<TEvent>();
+            _maxEvent = Activator.CreateInstance<TEvent>();
+        }
+
+        /// <summary>
+        /// Add animation updated methods
+        /// </summary>
+        protected virtual void OnEnable()
+        {
+            // Add delegates
+            if (Player != null)
+            {
+                Player.OnEventsUpdated += OnEventsUpdated;
+                Player.OnSampleUpdated += OnSampleUpdated;
+            }
+            // Refreshes event list & sample
+            RefreshEvents(true);
+        }
+
+        /// <summary>
+        /// Current events updated, refresh event list if needed
+        /// </summary>
+        protected virtual void OnEventsUpdated(TTSEventContainer eventContainer)
+        {
+            RefreshEvents(false);
+        }
+
+        /// <summary>
+        /// Current sample updated, refresh event if needed
+        /// </summary>
+        protected virtual void OnSampleUpdated(int newSample)
+        {
+            RefreshSample(false);
+        }
+
+        /// <summary>
+        /// Remove animation updated methods
+        /// </summary>
+        protected virtual void OnDisable()
+        {
+            // Remove delegates
+            if (Player != null)
+            {
+                Player.OnEventsUpdated -= OnEventsUpdated;
+                Player.OnSampleUpdated -= OnSampleUpdated;
+            }
+        }
+
+        /// <summary>
+        /// Updates animation event list
+        /// </summary>
+        protected virtual void RefreshEvents(bool force)
+        {
+            // Get events
+            var events = GetEvents();
+
+            // Ignore if desired
+            if (!force && !ShouldUpdateEvents(events))
+            {
+                return;
+            }
+
+            // Apply events
+            Events = events;
+
+            // Refresh sample
+            RefreshSample(true);
+        }
+
+        /// <summary>
+        /// Getter method for events, can be overwritten if needed
+        /// </summary>
+        protected virtual List<TEvent> GetEvents() => Player?.CurrentEvents?.GetEvents<TEvent>().ToList();
+
+        /// <summary>
+        /// Getter method for events, can be overwritten if needed
+        /// </summary>
+        protected virtual bool ShouldUpdateEvents(List<TEvent> newEvents) =>
+            Events == null || (newEvents != Events && newEvents != null && newEvents.Count != Events.Count);
+
+        /// <summary>
+        /// Updates currently set sample and lerps between events as specified
+        /// </summary>
+        /// <param name="force">If true, will force the sample set & lerping between events.
+        /// If false the sample will only be set if it has changed.</param>
+        protected virtual void RefreshSample(bool force)
+        {
+            // Get sample
+            var sample = Player == null || Events == null ? -1 : Player.CurrentSample;
+            if (!force && sample == _sample)
+            {
+                return;
+            }
+
+            // Store current sample
+            _sample = sample;
+
+            // Set to min event
+            if (_sample < 0)
+            {
+                LerpEvent(_minEvent, _minEvent, 0f);
+                return;
+            }
+
+            // Get to & from event
+            GetSampleEvents(_sample, out var fromEvent, out var toEvent);
+
+            // Determine percentage from 0 to 1
+            float percentage = GetSampleEventProgress(sample, fromEvent.SampleOffset, toEvent.SampleOffset);
+
+            // Lerp
+            LerpEvent(fromEvent, toEvent, percentage);
+        }
+
+        // Gets the events before and after a sample
+        private void GetSampleEvents(int sample, out TEvent previousEvent, out TEvent nextEvent)
+        {
+            // Set from & to event to the default for now
+            previousEvent = _minEvent;
+            nextEvent = _maxEvent;
+            nextEvent.offset = Player == null ? int.MaxValue : Player.TotalSamples;
+
+            // Ignore if invalid sample
+            foreach (var animEvent in Events)
+            {
+                if (animEvent.SampleOffset == sample)
+                {
+                    previousEvent = animEvent;
+                    nextEvent = animEvent;
+                    break;
+                }
+                // If less, check if could be closest from event
+                if (animEvent.SampleOffset < sample)
+                {
+                    if (animEvent.SampleOffset >= previousEvent.SampleOffset)
+                    {
+                        previousEvent = animEvent;
+                    }
+                }
+                // If more, check if could be closest to event
+                else
+                {
+                    if (animEvent.SampleOffset <= nextEvent.SampleOffset)
+                    {
+                        nextEvent = animEvent;
+                    }
+                }
+            }
+        }
+
+        // Get the progress from 0 to 1 from the previous event to the next event
+        private float GetSampleEventProgress(int sample, int previousEventSample, int nextEventSample)
+        {
+            // Start with 0
+            float progress = 0f;
+            // Only determine progress if not the same
+            if (previousEventSample != nextEventSample)
+            {
+                progress = Mathf.Clamp01((float)(sample - previousEventSample) / (nextEventSample - previousEventSample));
+            }
+            // Ignore ease altogether
+            if (easeIgnored)
+            {
+                progress = progress >= 1 ? 1f : 0f;
+            }
+            // Ease using the ease curve
+            else
+            {
+                progress = easeCurve.Evaluate(progress);
+            }
+            return progress;
+        }
+
+        /// <summary>
+        /// Performs a lerp from an event to another event
+        /// </summary>
+        /// <param name="fromEvent">The event starting point</param>
+        /// <param name="toEvent">The event ending point</param>
+        /// <param name="percentage">0 to 1 value with 0 meaning previous event & 1 meaning next event</param>
+        protected abstract void LerpEvent(TEvent fromEvent, TEvent toEvent, float percentage);
+    }
+}
---
+++ Features/TTS/Scripts/Runtime/LipSync/VisemeTextureFlipLipSync.cs
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Meta.WitAi.TTS.Data;
+using Meta.WitAi.TTS.Integrations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Meta.WitAi.TTS.LipSync
+{
+    /// <summary>
+    /// A class for swapping out mouth textures during an audio animation based on the current viseme
+    /// </summary>
+    public class VisemeTextureFlipLipSync : TTSEventAnimator<TTSVisemeEvent, Viseme>
+    {
+        /// <summary>
+        /// The material to be used for the texture flip
+        /// </summary>
+        public Material Material;
+
+        /// <summary>
+        /// The textures to be used for viseme swapping
+        /// </summary>
+        public VisemeTextureData[] VisemeTextures;
+        // A dictionary per viseme lookup
+        private Dictionary<Viseme, int> _textureLookup = new Dictionary<Viseme, int>();
+        // Current viseme
+        private Viseme _currentViseme = (Viseme)(-1);
+
+        // Stored texture data
+        [Serializable]
+        public struct VisemeTextureData
+        {
+            public Viseme viseme;
+            public Texture2D[] textures;
+        }
+
+        // Setup texture list
+        protected virtual void Reset()
+        {
+            if (VisemeTextures != null && VisemeTextures.Length > 0)
+            {
+                return;
+            }
+            List<VisemeTextureData> newTextures = new List<VisemeTextureData>();
+            foreach (Viseme v in Enum.GetValues(typeof(Viseme)))
+            {
+                newTextures.Add(new VisemeTextureData()
+                {
+                    viseme = v
+                });
+            }
+            VisemeTextures = newTextures.ToArray();
+        }
+
+        // Refresh texture lookup on awake
+        protected override void Awake()
+        {
+            base.Awake();
+            RefreshTextureLookup();
+            if (Material == null)
+            {
+                VLog.E(GetType().Name, "Texture Flip material unassigned");
+            }
+            SetViseme(Viseme.sil);
+        }
+
+        /// <summary>
+        /// Refreshes texture lookup
+        /// </summary>
+        public void RefreshTextureLookup()
+        {
+            // Ignore without textures
+            if (VisemeTextures == null)
+            {
+                return;
+            }
+
+            // Adds textures to dictionary lookup
+            StringBuilder log = new StringBuilder();
+            _textureLookup.Clear();
+            for (int i = 0; i < VisemeTextures.Length; i++)
+            {
+                Viseme v = VisemeTextures[i].viseme;
+                if (VisemeTextures[i].textures == null || VisemeTextures[i].textures.Length == 0)
+                {
+                    log.AppendLine($"VisemeTextures[{i}] Warning: No textures are set.");
+                    continue;
+                }
+                if (_textureLookup.ContainsKey(v))
+                {
+                    log.AppendLine($"VisemeTextures[{i}] Warning: Viseme '{v}' already used by VisemeTextures[{_textureLookup[v]}].");
+                    continue;
+                }
+                _textureLookup[v] = i;
+            }
+
+            // Logs missing viseme textures
+            CheckForMissingVisemes(log);
+
+            // Log warnings
+            if (log.Length > 0)
+            {
+                VLog.E(GetType().Name, $"Setup Warnings:\n{log}");
+            }
+        }
+
+        // Check for missing visemes & add them to the log
+        private void CheckForMissingVisemes(StringBuilder log)
+        {
+            foreach (Viseme v in Enum.GetValues(typeof(Viseme)))
+            {
+                if (!_textureLookup.ContainsKey(v))
+                {
+                    log.AppendLine($"{v} Viseme missing texture");
+                }
+            }
+        }
+
+        // Simply sets to the previous unless equal to the next
+        protected override void LerpEvent(TTSVisemeEvent fromEvent, TTSVisemeEvent toEvent, float percentage)
+        {
+            if (percentage < 1f)
+            {
+                SetViseme(fromEvent.Data);
+            }
+            else
+            {
+                SetViseme(toEvent.Data);
+            }
+        }
+
+        // Apply the specified viseme
+        private void SetViseme(Viseme v)
+        {
+            // If viseme is missing, try to use sil
+            if (!_textureLookup.ContainsKey(v))
+            {
+                if (v != Viseme.sil)
+                {
+                    SetViseme(Viseme.sil);
+                }
+                return;
+            }
+
+            // Ignore if same
+            if (_currentViseme == v)
+            {
+                return;
+            }
+            // Set current viseme
+            _currentViseme = v;
+
+            // Get texture index
+            int visemeIndex = _textureLookup[v];
+            int textureIndex = 0;
+            var vt = VisemeTextures[visemeIndex];
+            if (vt.textures.Length > 1)
+            {
+                textureIndex = Random.Range(0, vt.textures.Length);
+            }
+
+            // Apply texture
+            SetTexture(vt.textures[textureIndex]);
+        }
+
+        /// <summary>
+        /// Apply texture to material
+        /// </summary>
+        protected virtual void SetTexture(Texture2D texture)
+        {
+            if (Material != null)
+            {
+                Material.SetTexture("_MainTex", texture);
+            }
+        }
+    }
+}
