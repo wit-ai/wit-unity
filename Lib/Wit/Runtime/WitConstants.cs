d247751b1e465cf04640e433a008399e1edebf2f
Ryan Bailey <rybailey@meta.com>
2023-10-24 11:25:07 -0700

    Upgrading to v61 & API version
    Summary:
    1. Updates WitConstant sdk version to v61.0.0
    2. Updates WitConstant api version to 20231013
    3. Updates all package versions to 61.0.0 for github & internal testing
    
    Reviewed By: yolanother
    
    Differential Revision:
    D50560206
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 76a6831bbb91fb7e5154277ab26b065babd1d49b

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20230913";
-        public const string SDK_VERSION = "60.0.0";
+        public const string API_VERSION = "20231013";
+        public const string SDK_VERSION = "61.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
