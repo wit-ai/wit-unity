33a2c9a7bedd569cca444f8b3b6df358e36bb853
Aaron Jackson <akjackson@meta.com>
2023-10-27 08:47:35 -0700

    Add refresh and default view to DropDownAttribute
    Summary:
    Adds an optional refresh button and defaults to the regular field if no options are found. This allows devs to fill in the field manually if using a field that may have failed to fetch. Using refresh also has a secondary method that it can use to trigger a longer async fetch if needed vs the GUI refresh.
    
     {F1117411501}
    
    Reviewed By: ryanbailey-fb
    
    Differential Revision: D50209268
    
    fbshipit-source-id: adfda1dc0fcddfa17c7a3d93fdc7ae949a26e7fc

--- Lib/Wit/Runtime/Attributes/DropDownAttribute.cs
+++ Lib/Wit/Runtime/Attributes/DropDownAttribute.cs
@@ -40,15 +40,34 @@ namespace Meta.WitAi.Attributes
         /// this will always clamp the dropdown to the first option.
         /// </summary>
         public bool AllowInvalid { get; }
+        
+        /// <summary>
+        /// Returns true if the data options for the field list are empty and displays the property value instead.
+        /// </summary>
+        public bool ShowPropertyIfListIsEmpty { get; }
+        
+        /// <summary>
+        /// If true a refresh button will be shown
+        /// </summary>
+        public bool ShowRefreshButton { get; }
+        
+        /// <summary>
+        /// If set, this method will be used when the refresh button is pressed. This may be a longer manual refresh if
+        /// needed and may or may not be the same as the refresh method used for refreshing on paint.
+        /// </summary>
+        public string RefreshMethodName { get; }
 
         /// <summary>
         /// Constructor for drop down attribute
         /// </summary>
-        public DropDownAttribute(string optionListGetterName, bool refreshOnRepaint = false, bool allowInvalid = false)
+        public DropDownAttribute(string optionListGetterName, bool refreshOnRepaint = false, bool allowInvalid = false, bool showPropertyIfListIsEmpty = true, bool showRefreshButton = true, string refreshMethodName = null)
         {
             OptionListGetterName = optionListGetterName;
             RefreshOnRepaint = refreshOnRepaint;
             AllowInvalid = allowInvalid;
+            ShowPropertyIfListIsEmpty = showPropertyIfListIsEmpty;
+            ShowRefreshButton = showRefreshButton;
+            RefreshMethodName = refreshMethodName;
         }
     }
 
@@ -68,6 +87,14 @@ namespace Meta.WitAi.Attributes
         // Quick lookup for index
         private Dictionary<string, int> _optionLookup = new Dictionary<string, int>();
 
+        private readonly float _refreshSpace =
+            EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+
+        private readonly float _refreshButtonSize =
+            EditorGUIUtility.singleLineHeight - EditorGUIUtility.standardVerticalSpacing;
+
+        private MethodInfo _refreshMethod;
+
         // Layout string field as dropdown if possible
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
@@ -80,7 +107,7 @@ namespace Meta.WitAi.Attributes
 
             // Obtain method info if missing
             var dropDownAttribute = (DropDownAttribute)attribute;
-            if (_method == null && !RefreshMethod(property, dropDownAttribute))
+            if (!EnsureGetterMethod(property, dropDownAttribute))
             {
                 string typeName = property.serializedObject.targetObject.GetType().Name;
                 string methodName = dropDownAttribute.OptionListGetterName;
@@ -91,22 +118,81 @@ namespace Meta.WitAi.Attributes
                 return;
             }
 
-            // Obtain options/lookup if empty or required on repaint
-            if ((_options == null || dropDownAttribute.RefreshOnRepaint) && !RefreshOptions(property))
+            if (dropDownAttribute.ShowRefreshButton)
             {
-                EditorGUI.PropertyField(position, property, label);
-                return;
+                position.width -= _refreshSpace;
             }
 
-            // Layout dropdown
-            LayoutDropdown(position, property, label, dropDownAttribute);
+            // Obtain options/lookup if empty or required on repaint
+            if ((_options == null 
+                 || dropDownAttribute.ShowPropertyIfListIsEmpty && _options.Length == 0 
+                 || dropDownAttribute.RefreshOnRepaint) && !RefreshOptions(property))
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+            else
+            {
+                LayoutDropdown(position, property, label, dropDownAttribute);
+            }
+
+            if (dropDownAttribute.ShowRefreshButton)
+            {
+                position.width += _refreshSpace;
+
+                DrawRefreshButton(position, property, dropDownAttribute);
+            }
+        }
+
+        private void DrawRefreshButton(Rect position, SerializedProperty property, DropDownAttribute dropDownAttribute)
+        {
+            Rect buttonRect = new Rect(position.xMax - _refreshButtonSize, position.y,
+                _refreshButtonSize, _refreshButtonSize);
+
+            GUIContent iconContent = EditorGUIUtility.IconContent("d_Refresh");
+            iconContent.tooltip = "Refresh";
+
+            if (GUI.Button(buttonRect, iconContent, GUIStyle.none))
+            {
+                if (string.IsNullOrEmpty(dropDownAttribute.RefreshMethodName))
+                {
+                    RefreshOptions(property);
+                }
+                else
+                {
+                    FullRefresh(property);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Makes sure the main getter method has been resolved
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="dropDownAttribute"></param>
+        /// <returns></returns>
+        private bool EnsureGetterMethod(SerializedProperty property, DropDownAttribute dropDownAttribute)
+        {
+            return null != _method || ResolveGetterMethod(property, dropDownAttribute);
+        }
+
+        /// <summary>
+        /// Makes sure the refresh method has been resolved if needed
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="dropDownAttribute"></param>
+        /// <returns></returns>
+        private bool EnsureRefreshMethod(SerializedProperty property, DropDownAttribute dropDownAttribute)
+        {
+            // Refresh method is optional, so ensure can return true if it isn't set.
+            return string.IsNullOrEmpty(dropDownAttribute.RefreshMethodName) ||
+                   null != _refreshMethod || ResolveRefreshMethod(property, dropDownAttribute);
         }
 
         /// <summary>
         /// Attempts to obtain the MethodInfo for the specified method name
         /// </summary>
         /// <returns>Returns true if method is found that contains correct number of parameters & results</returns>
-        private bool RefreshMethod(SerializedProperty property, DropDownAttribute dropDownAttribute)
+        private bool ResolveGetterMethod(SerializedProperty property, DropDownAttribute dropDownAttribute)
         {
             Type baseType = property.serializedObject.targetObject.GetType();
             _method = baseType.GetMethod(dropDownAttribute.OptionListGetterName,
@@ -117,6 +203,22 @@ namespace Meta.WitAi.Attributes
             _options = null;
             _optionLookup.Clear();
             return _method != null && _method.GetParameters().Length == 0 && _method.ReturnType.IsAssignableFrom(typeof(IEnumerable<string>));
+        }
+
+        /// <summary>
+        /// Attempts to obtain the MethodInfo for the specified method name
+        /// </summary>
+        /// <returns>Returns true if method is found that contains correct number of parameters & results</returns>
+        private bool ResolveRefreshMethod(SerializedProperty property, DropDownAttribute dropDownAttribute)
+        {
+            if (null != _refreshMethod) return true;
+            Type baseType = property.serializedObject.targetObject.GetType();
+            _refreshMethod = baseType.GetMethod(dropDownAttribute.RefreshMethodName,
+                BindingFlags.Public
+                | BindingFlags.NonPublic
+                | BindingFlags.Instance
+                | BindingFlags.Static);
+            return _method != null;
         }
 
         /// <summary>
@@ -145,6 +247,17 @@ namespace Meta.WitAi.Attributes
 
             // Successful if more than one option exists
             return _options != null && _options.Length > 0;
+        }
+
+        /// <summary>
+        /// Performs the MethodInfo call, casts the options returned into an array & generates
+        /// an option lookup dictionary for easier index lookup.
+        /// </summary>
+        /// <returns>Returns true if more than one option is found</returns>
+        private bool FullRefresh(SerializedProperty property)
+        {
+            _refreshMethod.Invoke(property.serializedObject.targetObject, null);
+            return RefreshOptions(property);
         }
 
         /// <summary>
