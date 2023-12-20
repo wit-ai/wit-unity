27258b68c1397b9625a821d74c2a38049ce9a9f0
Ryan Bailey <rybailey@meta.com>
2023-12-20 13:59:08 -0800

    Update packages & WitConstants to v63
    Summary:
    1. Set WitConstants API version to 20231213
    2. Set WitConstants SDK version to v63
    3. Refresh all package.json to v63
    4. Remove TMPro requirement for VSTK to become compatible with Unity 2023
    5. Update packages-lock.json for all dev projects
    
    Reviewed By: yolanother
    
    Differential Revision:
    D52258970
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 7041e01de194dbc467ccc27365fd0489f5e28866

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20231113";
-        public const string SDK_VERSION = "62.0.0";
+        public const string API_VERSION = "20231213";
+        public const string SDK_VERSION = "63.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
