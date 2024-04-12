9457233482f22f2a7f1ba487141f0c80ed65770f
Ryan Bailey <rybailey@meta.com>
2024-04-12 13:10:50 -0700

    Addition of IPubSubSubscriber & PubSubSubscriptionState classes
    Summary:
    1. Adds PubSubSubscriptionState for the various states of a subscribed topic
    2. Adds IPubSubSubscriber subscription/unsubscription interface
    
    Reviewed By: Barbog
    
    Differential Revision:
    D55989715
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: e5a803431e5e3094bfb914f81f32aba8dc8ebfff

---
+++ Lib/Wit/Runtime/Net/PubSub/IPubSubSubscriber.cs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace Meta.Voice.Net.PubSub
+{
+    /// <summary>
+    /// A delegate callback for topic id and subscription state
+    /// </summary>
+    public delegate void PubSubTopicSubscriptionDelegate(string topicId, PubSubSubscriptionState subscriptionState);
+
+    /// <summary>
+    /// An interface for subscribing and unsubscribing from a specific topic
+    /// </summary>
+    public interface IPubSubSubscriber
+    {
+        /// <summary>
+        /// Callback when subscription state changes for a specific topic id
+        /// </summary>
+        event PubSubTopicSubscriptionDelegate OnTopicSubscriptionStateChange;
+
+        /// <summary>
+        /// Obtains the current subscription state for a specific topic
+        /// </summary>
+        PubSubSubscriptionState GetTopicSubscriptionState(string topicId);
+
+        /// <summary>
+        /// Method to subscribe to a specific topic id
+        /// </summary>
+        void Subscribe(string topicId);
+
+        /// <summary>
+        /// Method to unsubscribe from a specific topic id
+        /// </summary>
+        void Unsubscribe(string topicId);
+    }
+}
---
+++ Lib/Wit/Runtime/Net/PubSub/PubSubSubscriptionState.cs
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace Meta.Voice.Net.PubSub
+{
+    /// <summary>
+    /// An enum used to represent the current state of a topic's subscription
+    /// </summary>
+    public enum PubSubSubscriptionState
+    {
+        NotSubscribed,
+        Subscribing,
+        Subscribed,
+        Unsubscribing,
+        Error
+    }
+}
