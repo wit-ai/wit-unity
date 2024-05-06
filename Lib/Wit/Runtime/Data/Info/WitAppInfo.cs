ba1b1491da36133a4ad9ae3fefec74edcb50e67b
Ryan Bailey <rybailey@meta.com>
2024-05-06 12:57:55 -0700

    Removal of unused NPCAppInfo struct
    Summary: Removes unused NPCAppInfo struct
    
    Reviewed By: yolanother
    
    Differential Revision:
    D56897586
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 48c2deeb379fd32a5252fe022a12f3e2ae385c4d

--- Lib/Wit/Runtime/Data/Info/WitAppInfo.cs
+++ Lib/Wit/Runtime/Data/Info/WitAppInfo.cs
@@ -100,11 +100,4 @@ namespace Meta.WitAi.Data.Info
 
         public WitVoiceInfo[] voices;
     }
-
-    // Wit character app info.
-    [Serializable]
-    public struct NpcAppInfo
-    {
-        [SerializeField] public WitAppInfo[] data;
-    }
 }
