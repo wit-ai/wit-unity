d9737e4ab2626423eb98dedf9294c357a65ea820
Ryan Bailey <rybailey@meta.com>
2024-05-23 06:42:06 -0700

    Adds support for tts streaming of 32 bit, 64 bit and unsigned pcm
    Summary:
    1. Adds an enum for PCM type
    2. Adds pcm type to AudioDecoderPcm constructor
    3. Adds pcm decode methods for various options
    
    Reviewed By: yolanother
    
    Differential Revision:
    D57631709
    
    Privacy Context Container: L1079626
    
    fbshipit-source-id: 991341653372e2265a16b0d3925607b5904b7895

--- Lib/Wit/Runtime/Audio/Decoding/AudioDecoderPcm.cs
+++ Lib/Wit/Runtime/Audio/Decoding/AudioDecoderPcm.cs
@@ -14,25 +14,54 @@ using UnityEngine.Scripting;
 namespace Meta.Voice.Audio.Decoding
 {
     /// <summary>
+    /// Various pcm types
+    /// </summary>
+    public enum AudioDecoderPcmType
+    {
+        Int16,
+        Int32,
+        Int64,
+        UInt16,
+        UInt32,
+        UInt64
+    }
+
+    /// <summary>
     /// An audio decoder for raw PCM audio data
     /// </summary>
     [Preserve]
     public class AudioDecoderPcm : IAudioDecoder
     {
         #region INSTANCE
-        // PCM16 uses 2 bytes
-        private const int _byteCount = 2;
+        // Provides information on current pcm type
+        public readonly AudioDecoderPcmType PcmType;
+        // Total bytes per pcm sample
+        private readonly int _byteCount;
+        // Decode method from bytes to float
+        private Func<byte[], int, float> _decoder;
 
         // Storage of overflow bytes
         private int _overflowOffset = 0;
-        private readonly byte[] _overflow = new byte[_byteCount];
+        private readonly byte[] _overflow;
         private readonly List<float> _samples = new List<float>();
 
         /// <summary>
         /// Default constructor for PCM16
         /// </summary>
         [Preserve]
-        public AudioDecoderPcm() {}
+        public AudioDecoderPcm() : this(AudioDecoderPcmType.Int16) {}
+
+        /// <summary>
+        /// Constructor that allows selection of pcm type
+        /// </summary>
+        [Preserve]
+        public AudioDecoderPcm(AudioDecoderPcmType pcmType)
+        {
+            PcmType = pcmType;
+            _byteCount = GetByteCount(PcmType);
+            _overflow = new byte[_byteCount];
+            _decoder =  GetDecodeMethod(PcmType);
+        }
 
         /// <summary>
         /// Once setup this should display the number of channels expected to be decoded
@@ -81,7 +110,7 @@ namespace Meta.Voice.Audio.Decoding
                 Array.Copy(buffer, bufferOffset, _overflow, _overflowOffset, overflowLength);
 
                 // Decode and add overflow sample
-                var sample = DecodePCM16Sample(_overflow, 0);
+                var sample = _decoder(_overflow, 0);
                 _samples.Add(sample);
 
                 // Increment buffer offset/decrement length
@@ -93,7 +122,7 @@ namespace Meta.Voice.Audio.Decoding
             // Decode and append while there are enough for a sample
             while (bufferLength >= _byteCount)
             {
-                var sample = DecodePCM16Sample(buffer, bufferOffset);
+                var sample = _decoder(buffer, bufferOffset);
                 bufferOffset += _byteCount;
                 bufferLength -= _byteCount;
                 _samples.Add(sample);
@@ -113,35 +142,130 @@ namespace Meta.Voice.Audio.Decoding
 
         #region STATIC
         /// <summary>
+        /// Returns total bytes per sample
+        /// </summary>
+        public static int GetByteCount(AudioDecoderPcmType pcmType)
+        {
+            switch (pcmType)
+            {
+                case AudioDecoderPcmType.Int16:
+                case AudioDecoderPcmType.UInt16:
+                    return 2;
+                case AudioDecoderPcmType.UInt32:
+                case AudioDecoderPcmType.Int32:
+                    return 4;
+                case AudioDecoderPcmType.UInt64:
+                case AudioDecoderPcmType.Int64:
+                    return 8;
+            }
+            return 0;
+        }
+
+        /// <summary>
         /// Gets pcm sample count from byte content length (1 sample = 2 bytes)
         /// </summary>
         /// <param name="contentLength">The provided number of bytes</param>
-        public static long GetTotalSamplesPCM(long contentLength)
-            => contentLength / 2;
+        public static long GetTotalSamplesPcm(long contentLength, AudioDecoderPcmType pcmType = AudioDecoderPcmType.Int16)
+            => contentLength / GetByteCount(pcmType);
 
         /// <summary>
         /// Decodes an array of pcm data
         /// </summary>
         /// <param name="rawData">Raw array of pcm bytes</param>
-        public static float[] DecodePCM(byte[] rawData)
+        /// <param name="pcmType">The pcm decode type to be used</param>
+        public static float[] DecodePcm(byte[] rawData, AudioDecoderPcmType pcmType = AudioDecoderPcmType.Int16)
         {
-            int totalSamples = (int)GetTotalSamplesPCM(rawData.Length);
+            var decoder = GetDecodeMethod(pcmType);
+            int totalSamples = (int)GetTotalSamplesPcm(rawData.Length, pcmType);
             float[] samples = new float[totalSamples];
             for (int i = 0; i < samples.Length; i++)
             {
-                samples[i] = DecodePCM16Sample(rawData, i * 2);
+                samples[i] = decoder(rawData, i * 2);
             }
             return samples;
         }
 
         /// <summary>
-        /// Decodes a sample into a float from 0 to 1
+        /// Returns a decode method depending on the pcm type
+        /// </summary>
+        public static Func<byte[], int, float> GetDecodeMethod(AudioDecoderPcmType pcmType)
+        {
+            switch (pcmType)
+            {
+                case AudioDecoderPcmType.Int16:
+                    return DecodeSample_Pcm16;
+                case AudioDecoderPcmType.Int32:
+                    return DecodeSample_Pcm32;
+                case AudioDecoderPcmType.Int64:
+                    return DecodeSample_Pcm64;
+                case AudioDecoderPcmType.UInt16:
+                    return DecodeSample_PcmU16;
+                case AudioDecoderPcmType.UInt32:
+                    return DecodeSample_PcmU32;
+                case AudioDecoderPcmType.UInt64:
+                    return DecodeSample_PcmU64;
+            }
+            return DecodeSample_Pcm16;
+        }
+
+        /// <summary>
+        /// Decodes an Int16 sample into a float from 0 to 1
         /// </summary>
         /// <param name="rawData">Raw data to be decoded into a single sample</param>
         /// <param name="index">Offset of the data</param>
-        public static float DecodePCM16Sample(byte[] rawData, int index)
+        public static float DecodeSample_Pcm16(byte[] rawData, int index)
         {
             return (float)BitConverter.ToInt16(rawData, index) / Int16.MaxValue;
+        }
+
+        /// <summary>
+        /// Decodes an Int32 sample into a float from 0 to 1
+        /// </summary>
+        /// <param name="rawData">Raw data to be decoded into a single sample</param>
+        /// <param name="index">Offset of the data</param>
+        public static float DecodeSample_Pcm32(byte[] rawData, int index)
+        {
+            return (float)BitConverter.ToInt32(rawData, index) / Int32.MaxValue;
+        }
+
+        /// <summary>
+        /// Decodes an Int64 sample into a float from 0 to 1
+        /// </summary>
+        /// <param name="rawData">Raw data to be decoded into a single sample</param>
+        /// <param name="index">Offset of the data</param>
+        public static float DecodeSample_Pcm64(byte[] rawData, int index)
+        {
+            return (float)((double)BitConverter.ToInt64(rawData, index) / Int64.MaxValue);
+        }
+
+        /// <summary>
+        /// Decodes an UInt16 sample into a float from 0 to 1
+        /// </summary>
+        /// <param name="rawData">Raw data to be decoded into a single sample</param>
+        /// <param name="index">Offset of the data</param>
+        public static float DecodeSample_PcmU16(byte[] rawData, int index)
+        {
+            return (float)BitConverter.ToUInt16(rawData, index) / UInt16.MaxValue;
+        }
+
+        /// <summary>
+        /// Decodes an UInt32 sample into a float from 0 to 1
+        /// </summary>
+        /// <param name="rawData">Raw data to be decoded into a single sample</param>
+        /// <param name="index">Offset of the data</param>
+        public static float DecodeSample_PcmU32(byte[] rawData, int index)
+        {
+            return (float)BitConverter.ToUInt32(rawData, index) / UInt32.MaxValue;
+        }
+
+        /// <summary>
+        /// Decodes an UInt64 sample into a float from 0 to 1
+        /// </summary>
+        /// <param name="rawData">Raw data to be decoded into a single sample</param>
+        /// <param name="index">Offset of the data</param>
+        public static float DecodeSample_PcmU64(byte[] rawData, int index)
+        {
+            return (float)((double)BitConverter.ToUInt64(rawData, index) / UInt64.MaxValue);
         }
         #endregion
     }
