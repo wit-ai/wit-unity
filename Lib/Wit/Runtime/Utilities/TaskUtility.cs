15330e451f1e498a7a6d0f71e065ed3272b0bf32
Ryan Bailey <rybailey@meta.com>
2024-07-11 14:31:21 -0700

    Remove the TaskUtility.WaitWhile and TaskUtility.Wait methods
    Summary: Removing methods with poor performance due to rapid creation of tasks
    
    Reviewed By: MokhtarKhorshid
    
    Differential Revision: D59589698
    
    fbshipit-source-id: 45d21422685b7a8c1778a50830ed60f38cef91ef

--- Lib/Wit/Runtime/Utilities/TaskUtility.cs
+++ Lib/Wit/Runtime/Utilities/TaskUtility.cs
@@ -13,64 +13,12 @@ using UnityEngine;
 namespace Meta.WitAi
 {
     /// <summary>
-    /// A static class used for frequently used task methods
+    /// A static class used for assistance in various async await scenarios
     /// </summary>
     public static class TaskUtility
     {
         /// <summary>
-        /// The default cpu task delay time in ms
-        /// </summary>
-        public const int DELAY_DEFAULT = -1;
-
-        /// <summary>
-        /// Asynchronous task to be used for freeing up the CPU during tasks
-        /// </summary>
-        /// <param name="condition">Condition method that this method will keep waiting for, until false.</param>
-        /// <param name="delay">Delay time per condition check in ms</param>
-        public static async Task WaitWhile(Func<bool> condition, int delay = DELAY_DEFAULT)
-        {
-            do
-            {
-                try
-                {
-                    // Exit immediately if condition is false
-                    if (!condition.Invoke())
-                    {
-                        return;
-                    }
-                }
-                catch (Exception e)
-                {
-                    // Exit if exception occurs
-                    VLog.E(nameof(TaskUtility), $"Exception while running WaitWhile condition:\n{e}");
-                    return;
-                }
-
-                // Perform a wait
-                await Wait();
-            } while (true);
-        }
-
-        /// <summary>
-        /// Asynchronous task to be used for freeing up the CPU during looping tasks
-        /// </summary>
-        /// <param name="delay">Delay time per condition check in ms</param>
-        public static async Task Wait(int delay = DELAY_DEFAULT)
-        {
-            // Delay for specified amount of time
-            if (delay > 0)
-            {
-                await Task.Delay(delay);
-            }
-            // Delay for a yield
-            else
-            {
-                await Task.Yield();
-            }
-        }
-
-        /// <summary>
-        /// Awaits an IAsyncResult using Task.Factory.AsyncWa
+        /// Awaits an IAsyncResult using Task.Factory.FromAsync
         /// </summary>
         public static Task FromAsyncResult(IAsyncResult asyncResult)
         {
