ef6fb19002baa6794d84a7871c2e86345400226c
Ryan Bailey <rybailey@meta.com>
2025-02-14 14:45:28 -0800

    Updating packages to v77
    Summary: Updating package versions using: fbsource\arvr\assistant\sdk\unity\set_package_versions.sh
    
    Reviewed By: yolanother
    
    Differential Revision: D69596363
    
    fbshipit-source-id: 8c0a0475c0f68cc49a704fdb1b1d9b35eac4eac0

--- Lib/Wit/Runtime/WitConstants.cs
+++ Lib/Wit/Runtime/WitConstants.cs
@@ -11,8 +11,8 @@ namespace Meta.WitAi
     public static class WitConstants
     {
         // Wit service version info
-        public const string API_VERSION = "20240813";
-        public const string SDK_VERSION = "71.0.0";
+        public const string API_VERSION = "20250213";
+        public const string SDK_VERSION = "77.0.0";
         public const string CLIENT_NAME = "wit-unity";
 
         // Wit service endpoint info
